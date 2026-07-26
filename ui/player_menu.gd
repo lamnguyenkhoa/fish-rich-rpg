b1614df772f8bb222a38f4ea841c1e2b827a6438
NKLam <khoalamvn@gmail.com>
2026-07-26 23:32:07 +0700

    Add retry button

--- ui/player_menu.gd
+++ ui/player_menu.gd
@@ -3,8 +3,10 @@ class_name PlayerMenu
 
 @onready var tab_container: TabContainer = $TabContainer
 @onready var inventory_menu: InventoryMenu = $TabContainer/Inventory
+@onready var retry_button: Button = $RetryButton
 
 var player: Player = null
+var press_retry_once = false
 
 func _ready():
 	GameManager.player_menu = self
@@ -30,8 +32,12 @@ func open_menu():
 	inventory_menu.refresh_stat()
 	inventory_menu.refresh_inventory_data()
 	SoundManager.play_button_click_sfx()
+	press_retry_once = false
+	retry_button.text = "Retry"
 
 func close_menu():
+	press_retry_once = false
+	retry_button.text = "Retry"
 	visible = false
 
 func _on_close_button_pressed() -> void:
@@ -46,3 +52,12 @@ func _on_tab_container_tab_clicked(_tab: int) -> void:
 
 func _on_close_button_mouse_entered() -> void:
 	SoundManager.play_button_hover_sfx()
+
+
+func _on_retry_button_pressed() -> void:
+	if press_retry_once:
+		GameManager.reset()
+		get_tree().reload_current_scene()
+	else:
+		press_retry_once = true
+		retry_button.text = "Sure?"
